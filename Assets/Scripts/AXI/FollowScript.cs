e0c7521fca514dbb62961feb881fc0107978402f
AntLayNot <a.garcia@rns.ecolescreatives.com>
2026-04-30 17:56:29 +0200

    v.007.4

---
+++ Assets/Scripts/AXI/FollowScript.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class FollowScript : MonoBehaviour
+{
+    [SerializeField] private Transform target_t;
+    private Animator animator;
+    private Rigidbody2D rb;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
